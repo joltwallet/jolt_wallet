28f087506c83178689cf07c713d94f363ec8dcbc
James Coxon <175479+jamescoxon@users.noreply.github.com>
2018-05-01 20:26:02 +0100

    removed wifi settings from example_wifi

--- main/example_wifi.h
+++ main/example_wifi.h
@@ -2,10 +2,10 @@
 #define __INCLUDE_WIFI_H__
 
 // The identity of the access point to which we wish to connect.
-#define AP_TARGET_SSID     "Sherlock"
+#define AP_TARGET_SSID     ""
 
 // The password we need to supply to the access point for authorization.
-#define AP_TARGET_PASSWORD "seashore"
+#define AP_TARGET_PASSWORD ""
 
 void wifi_connect();
 void get_ap_info(char * ssid_info);
