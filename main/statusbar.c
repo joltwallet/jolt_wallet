d5c0172cf91fb191b171825cd27b3bb77081a679
Brian Pugh <bnp117@gmail.com>
2018-05-05 22:08:47 -0700

    statusbar: dummy bluetooth; dummy battery

--- main/statusbar.c
+++ main/statusbar.c
@@ -14,6 +14,8 @@
 #include "statusbar.h"
 
 #define STATUSBAR_UPDATE_PERIOD_MS 2000
+#define STATUSBAR_ANCHOR_X 90
+#define STATUSBAR_PAD 2
 
 static const char* TAG = "StatusBar";
 
@@ -22,8 +24,8 @@ bool statusbar_draw_enable = true;
 
 static void statusbar_wifi(menu8g2_t *menu){
     /* Call in a drawing loop */
-    const uint16_t x = 100;
-    const uint16_t y = 0;
+    const uint16_t x = STATUSBAR_ANCHOR_X;
+    const uint16_t y = 1;
     const unsigned char *graphic;
     uint8_t wifi_strength = get_wifi_strength();
     switch(wifi_strength){
@@ -49,8 +51,57 @@ static void statusbar_wifi(menu8g2_t *menu){
             graphic);
 }
 
+static void statusbar_bluetooth(menu8g2_t *menu){
+    const uint16_t x = STATUSBAR_ANCHOR_X + GRAPHIC_WIFI_W 
+            + 1 * STATUSBAR_PAD;
+    const uint16_t y = 0;
+    bool bluetooth_status = true; // todo: read bluetooth status
+
+    if (bluetooth_status){
+        u8g2_SetDrawColor(menu->u8g2, 0);
+        u8g2_DrawBox( menu->u8g2, x, y,
+                GRAPHIC_BLUETOOTH_W, GRAPHIC_BLUETOOTH_H);
+        u8g2_SetDrawColor(menu->u8g2, 1);
+        u8g2_DrawXBM( menu->u8g2, x, y,
+                GRAPHIC_BLUETOOTH_W,
+                GRAPHIC_BLUETOOTH_H,
+                graphic_bluetooth);
+    }
+}
+
+static void statusbar_battery(menu8g2_t *menu){
+    const uint16_t x = STATUSBAR_ANCHOR_X + GRAPHIC_WIFI_W + GRAPHIC_BLUETOOTH_W
+            + 2 * STATUSBAR_PAD;
+    const uint16_t y = 1;
+    const unsigned char *graphic;
+    uint8_t battery_strength = 0; //todo: read battery voltage
+    switch(battery_strength){
+        case 1:
+            graphic = graphic_battery_1;
+            break;
+        case 2:
+            graphic = graphic_battery_2;
+            break;
+        case 3:
+            graphic = graphic_battery_3;
+            break;
+        default:
+            graphic = graphic_battery_0;
+            break;
+    }
+    u8g2_SetDrawColor(menu->u8g2, 0);
+    u8g2_DrawBox( menu->u8g2, x, y, GRAPHIC_BATTERY_W, GRAPHIC_BATTERY_H);
+    u8g2_SetDrawColor(menu->u8g2, 1);
+    u8g2_DrawXBM( menu->u8g2, x, y,
+            GRAPHIC_BATTERY_W,
+            GRAPHIC_BATTERY_H,
+            graphic);
+}
+
 void statusbar_update(menu8g2_t *menu){
     statusbar_wifi(menu);
+    statusbar_bluetooth(menu);
+    statusbar_battery(menu);
 }
 
 void statusbar_task(void *menu_in){
