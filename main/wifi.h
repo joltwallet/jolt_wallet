9a8ecac3624947c6de478119e98d1367e4d54824
James Coxon <175479+jamescoxon@users.noreply.github.com>
2018-05-13 21:18:44 +0100

    moved wifi credentials to menuconfig/Kconfig, removed example wifi and added permanant wifi.h

--- main/wifi.h
+++ main/wifi.h
@@ -2,10 +2,10 @@
 #define __INCLUDE_WIFI_H__
 
 // The identity of the access point to which we wish to connect.
-#define AP_TARGET_SSID     ""
+//#define AP_TARGET_SSID     ""
 
 // The password we need to supply to the access point for authorization.
-#define AP_TARGET_PASSWORD ""
+//#define AP_TARGET_PASSWORD ""
 
 void wifi_connect();
 void get_ap_info(char * ssid_info);
