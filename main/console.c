a32970f0bb0a2d5b619d98540863580b035c8129
Brian Pugh <bnp117@gmail.com>
2018-05-26 20:49:48 -0700

    Made console a task

--- main/console.c
+++ main/console.c
@@ -3,6 +3,9 @@
 #include "globals.h"
 #include "loading.h"
 #include "sodium.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "freertos/task.h"
 
 #include "esp_console.h"
 #include "driver/uart.h"
@@ -18,46 +21,9 @@
 
 static const char* TAG = "console";
 
-void initialize_console()
-{
-    /* Disable buffering on stdin and stdout */
-    setvbuf(stdin, NULL, _IONBF, 0);
-    setvbuf(stdout, NULL, _IONBF, 0);
-    
-    /* Minicom, screen, idf_monitor send CR when ENTER key is pressed */
-    esp_vfs_dev_uart_set_rx_line_endings(ESP_LINE_ENDINGS_CR);
-    /* Move the caret to the beginning of the next line on '\n' */
-    esp_vfs_dev_uart_set_tx_line_endings(ESP_LINE_ENDINGS_CRLF);
-    
-    /* Install UART driver for interrupt-driven reads and writes */
-    ESP_ERROR_CHECK( uart_driver_install(CONFIG_CONSOLE_UART_NUM,
-                                         256, 0, 0, NULL, 0) );
-    
-    /* Tell VFS to use UART driver */
-    esp_vfs_dev_uart_use_driver(CONFIG_CONSOLE_UART_NUM);
-    
-    /* Initialize the console */
-    esp_console_config_t console_config = {
-        .max_cmdline_args = 8,
-        .max_cmdline_length = 256,
-    };
-    ESP_ERROR_CHECK( esp_console_init(&console_config) );
-    
-    /* Configure linenoise line completion library */
-    /* Enable multiline editing. If not set, long commands will scroll within
-     * single line.
-     */
-    linenoiseSetMultiLine(1);
-    
-    /* Tell linenoise where to get command completions and hints */
-    linenoiseSetCompletionCallback(&esp_console_get_completion);
-    linenoiseSetHintsCallback((linenoiseHintsCallback*) &esp_console_get_hint);
-    
-    /* Set command history size */
-    linenoiseHistorySetMaxLen(100);
-    
+volatile TaskHandle_t console_h = NULL;
 
-}
+
 
 static int free_mem(int argc, char** argv)
 {
@@ -180,39 +146,24 @@ double nano_rpc_balance(){
             break;
     }
     
-    loading_disable();
-    
-    return display_amount;
-    
 exit:
     loading_disable();
-    return;
+    return display_amount;
 
 }
 
-void menu_console(menu8g2_t *prev){
-    
-    esp_log_level_set("*", ESP_LOG_NONE);
-    
-    //initialize_console();
-
-    
-    /* Register commands */
-    esp_console_register_help_command();
-    register_free();
-    register_nano_count();
-    register_nano_balance();
-    
+void console_task(){
     /* Prompt to be printed before each line.
      * This can be customized, made dynamic, etc.
      */
     const char* prompt = "esp32> ";
-    
+
+    esp_log_level_set("*", ESP_LOG_NONE);
     printf("\n"
            "Welcome to the Jolt Console.\n"
            "Type 'help' to get the list of commands.\n"
            "\n");
-    
+
     /* Figure out if the terminal supports escape sequences */
     int probe_status = linenoiseProbe();
     if (probe_status) { /* zero indicates success */
@@ -224,7 +175,7 @@ void menu_console(menu8g2_t *prev){
     }
     
     /* Main loop */
-    while(true) {
+    for(;;) {
         /* Get a line using linenoise.
          * The line is returned when ENTER is pressed.
          */
@@ -256,7 +207,32 @@ void menu_console(menu8g2_t *prev){
     }
     
     esp_log_level_set("*", CONFIG_LOG_DEFAULT_LEVEL);
+    console_h = NULL;
+    vTaskDelete( NULL );
+}
 
+void start_console(){
+    xTaskCreate(console_task,
+                "ConsoleTask", 32000,
+                NULL, 11,
+                (TaskHandle_t *) &console_h);
+}
+
+void menu_console(menu8g2_t *prev){
+    menu8g2_t menu;
+    menu8g2_copy(&menu, prev);
+
+    if(console_h){
+        menu8g2_display_text_title(&menu,
+                "Console is alread running.",
+                "Console");
+    }
+    else{
+        start_console();
+        menu8g2_display_text_title(&menu,
+                "Console Started.",
+                "Console");
+    }
 }
 
 backend_rpc_response_t backend_rpc(backend_rpc_t *rpc){
@@ -330,3 +306,48 @@ void backend_task(void *backend_in){
         }
     }
 }
+
+void initialize_console() {
+    /* Disable buffering on stdin and stdout */
+    setvbuf(stdin, NULL, _IONBF, 0);
+    setvbuf(stdout, NULL, _IONBF, 0);
+    
+    /* Minicom, screen, idf_monitor send CR when ENTER key is pressed */
+    esp_vfs_dev_uart_set_rx_line_endings(ESP_LINE_ENDINGS_CR);
+    /* Move the caret to the beginning of the next line on '\n' */
+    esp_vfs_dev_uart_set_tx_line_endings(ESP_LINE_ENDINGS_CRLF);
+    
+    /* Install UART driver for interrupt-driven reads and writes */
+    ESP_ERROR_CHECK( uart_driver_install(CONFIG_CONSOLE_UART_NUM,
+                                         256, 0, 0, NULL, 0) );
+    
+    /* Tell VFS to use UART driver */
+    esp_vfs_dev_uart_use_driver(CONFIG_CONSOLE_UART_NUM);
+    
+    /* Initialize the console */
+    esp_console_config_t console_config = {
+        .max_cmdline_args = 8,
+        .max_cmdline_length = 256,
+    };
+    ESP_ERROR_CHECK( esp_console_init(&console_config) );
+    
+    /* Configure linenoise line completion library */
+    /* Enable multiline editing. If not set, long commands will scroll within
+     * single line.
+     */
+    linenoiseSetMultiLine(1);
+    
+    /* Tell linenoise where to get command completions and hints */
+    linenoiseSetCompletionCallback(&esp_console_get_completion);
+    linenoiseSetHintsCallback((linenoiseHintsCallback*) &esp_console_get_hint);
+    
+    /* Set command history size */
+    linenoiseHistorySetMaxLen(20);
+
+    /* Register commands */
+    esp_console_register_help_command();
+    register_free();
+    register_nano_count();
+    register_nano_balance();
+}
+
