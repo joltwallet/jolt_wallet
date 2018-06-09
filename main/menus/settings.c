744ef8e8a0f84ed490cb7dd1c1763217fc2f1fab
Brian Pugh <bnp117@gmail.com>
2018-06-08 21:47:00 -0700

    removed mnemonic_restore menu option

--- main/menus/settings.c
+++ main/menus/settings.c
@@ -64,11 +64,10 @@ void menu_settings(menu8g2_t *prev){
     const char title[] = "Settings";
 
     menu8g2_elements_t elements;
-    menu8g2_elements_init(&elements, 5);
+    menu8g2_elements_init(&elements, 4);
     menu8g2_set_element(&elements, "Screen Brightness", NULL);
     menu8g2_set_element(&elements, "WiFi Details", &wifi_details);
     menu8g2_set_element(&elements, "Bluetooth", NULL);
-    menu8g2_set_element(&elements, "Mnemonic Restore", &menu_mnemonic_restore);
     menu8g2_set_element(&elements, "Factory Reset", &menu_factory_reset);
     menu8g2_create_vertical_element_menu(&menu, title, &elements);
     menu8g2_elements_free(&elements);
--- main/menus/submenus.h
+++ main/menus/submenus.h
@@ -20,7 +20,6 @@
 #define __JOLT_SUBMENUS_H__
 
 void menu_settings(menu8g2_t *prev);
-void menu_mnemonic_restore(menu8g2_t *prev);
 void menu_console(menu8g2_t *prev);
 
 #endif
