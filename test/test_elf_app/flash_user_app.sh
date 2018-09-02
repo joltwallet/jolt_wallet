ee5e2845d094fa09113d3f72d6e90eeb233e62b3
Brian Pugh <bnp117@gmail.com>
2018-09-02 14:08:22 -0700

    Removed old test files

--- test/test_elf_app/flash_user_app.sh
+++
@@ -1,2 +0,0 @@
-USER_PARTITION_OFFSET=0x1f4000
-python2 /home/bpugh/esp/esp-idf/components/esptool_py/esptool/esptool.py --chip esp32 --port /dev/ttyUSB0 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size 4MB ${USER_PARTITION_OFFSET} main.elf
