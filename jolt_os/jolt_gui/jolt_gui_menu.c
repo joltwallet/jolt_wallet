b339c6e77ed081114762da371e87dc4e19ea0d10
Brian Pugh <bnp117@gmail.com>
2019-01-03 21:38:42 -0800

    Scroll only on selected

--- jolt_os/jolt_gui/jolt_gui_menu.c
+++ jolt_os/jolt_gui/jolt_gui_menu.c
@@ -8,16 +8,66 @@
  *   +--CONT_BODY
  *       +--LIST
  */
+#define LABEL_LONG_MODE_DEFAULT LV_LABEL_LONG_DOT
+#define LABEL_LONG_MODE_SELECTED LV_LABEL_LONG_ROLL 
 static const char TAG[] = "scr_menu";
+
+static lv_signal_func_t old_list_signal = NULL;     /*Store the old signal function*/
+
+static void set_selected_label_long_mode(lv_obj_t *list, lv_label_long_mode_t mode){
+    lv_obj_t *btn = lv_list_get_btn_selected(list);
+    if( NULL == btn ){
+        return;
+    }
+    lv_obj_t *label = lv_list_get_btn_label(btn);
+    if( NULL == label ){
+        return;
+    }
+    lv_label_set_long_mode(label, mode);
+}
+
+static lv_res_t new_list_signal(lv_obj_t *list, lv_signal_t sign, void * param)
+{
+    lv_res_t res;
+    char c;
+    
+
+    if(sign == LV_SIGNAL_FOCUS) {
+        // call ancestor
+        res = old_list_signal(list, sign, param);
+        if(res != LV_RES_OK) return res;
+        set_selected_label_long_mode(list, LABEL_LONG_MODE_SELECTED);
+    }
+    else if(sign == LV_SIGNAL_CONTROLL
+                && (c = *((char *)param), 
+                        (c == LV_GROUP_KEY_RIGHT || c == LV_GROUP_KEY_DOWN
+                        || c == LV_GROUP_KEY_LEFT || c == LV_GROUP_KEY_UP)) ){
+        set_selected_label_long_mode(list, LABEL_LONG_MODE_DEFAULT);
+        res = old_list_signal(list, sign, param);
+        if(res != LV_RES_OK) return res;
+        set_selected_label_long_mode(list, LABEL_LONG_MODE_SELECTED);
+    }
+    else{
+        res = old_list_signal(list, sign, param);
+        if(res != LV_RES_OK) return res;
+    }
+
+    return res;
+}
 
 /* Creates a standard Jolt Menu Screen */
 lv_obj_t *jolt_gui_scr_menu_create(const char *title) {
     JOLT_GUI_SCR_PREAMBLE( title );
 
+
     /* Create List */
     // todo, get these values from parent
     lv_obj_t *menu = lv_list_create(cont_body, NULL);
     JOLT_GUI_OBJ_CHECK(menu);
+    if ( NULL == old_list_signal ) {
+        old_list_signal = lv_obj_get_signal_func(menu);
+    }
+    lv_obj_set_signal_func(menu, new_list_signal);
     lv_obj_set_free_num(menu, JOLT_GUI_OBJ_ID_LIST);
     lv_obj_set_size(menu,
             lv_obj_get_width(cont_body), lv_obj_get_height(cont_body));
@@ -49,15 +99,22 @@ exit:
 /* Adds an item to a Jolt Menu Screen */
 lv_obj_t *jolt_gui_scr_menu_add(lv_obj_t *par, const void *img_src,
         const char *txt, lv_action_t rel_action) {
-    lv_obj_t *btn = NULL;
     lv_obj_t *list = jolt_gui_scr_menu_get_list( par );
     if( NULL == list ) {
         return NULL;
     }
-    btn = lv_list_add(list, img_src, txt, rel_action);
+
+    lv_obj_t *btn = lv_list_add(list, img_src, txt, rel_action);
     if( NULL == btn ){
         goto exit;
     }
+
+    lv_obj_t *label = lv_list_get_btn_label(btn);
+    if( NULL == btn ){
+        goto exit;
+    }
+    lv_label_set_long_mode(label, LABEL_LONG_MODE_DEFAULT);
+    
 
 exit:
     return btn;
