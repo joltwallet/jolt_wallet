53f5f59c1321457c3d501eca42fe67ee2c85f6cf
Brian Pugh <bnp117@gmail.com>
2019-02-09 19:59:48 -0800

    Proper slider pos finding with inverted LED

--- jolt_os/jolt_gui/menus/settings/led_brightness.c
+++ jolt_os/jolt_gui/menus/settings/led_brightness.c
@@ -15,7 +15,7 @@ static const uint8_t brightness_levels[] = {0, 10, 30, 70, 150, 255};
 
 static uint8_t led_brightness_get() {
     uint8_t val;
-    storage_get_u8(&val, "user", "led_val", 70 );
+    storage_get_u8(&val, "user", "led_val", CONFIG_JOLT_TOUCH_LED_DEFAULT_DUTY );
     return val;
 }
 
@@ -35,27 +35,26 @@ static lv_res_t led_brightness_update(lv_obj_t *slider) {
     return LV_RES_OK;
 }
 
-static lv_res_t led_brightness_back(lv_obj_t *btn) {
-    jolt_gui_scr_del();
-    uint8_t val = led_brightness_get();
-    jolt_led_set(val);
-    return LV_RES_INV;
-}
-
 lv_res_t menu_led_brightness_create() {
     uint8_t brightness = led_brightness_get();
     int16_t slider_pos;
     ESP_LOGI(TAG, "Stored brightness: %d", brightness);
     for(slider_pos=0; slider_pos < sizeof(brightness_levels); slider_pos++) {
+#if CONFIG_JOLT_TOUCH_LED_INVERT
+        if(brightness >= brightness_levels[slider_pos]) {
+            break;
+        }
+#else
         if(brightness <= brightness_levels[slider_pos]) {
             break;
         }
+#endif
     }
 
     lv_obj_t *scr = jolt_gui_scr_slider_create(gettext(JOLT_TEXT_BRIGHTNESS), NULL, led_brightness_update);
     jolt_gui_scr_slider_set_range(scr, 0, sizeof(brightness_levels)-1);
     jolt_gui_scr_slider_set_value(scr, slider_pos);
-    jolt_gui_scr_set_back_action(scr, led_brightness_back);
+    jolt_gui_scr_set_back_action(scr, led_brightness_save);
     jolt_gui_scr_set_enter_action(scr, led_brightness_save);
     return LV_RES_OK;
 }
