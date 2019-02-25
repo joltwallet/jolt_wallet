f7ca0a287c25a5e7ba37d67486e8e55efe3815ac
Brian Pugh <bnp117@gmail.com>
2019-02-25 09:10:04 -0800

    usb_upload.py increase verbosity and speed

--- elf2jelf/usb_upload.py
+++ elf2jelf/usb_upload.py
@@ -22,13 +22,15 @@ def main(args):
     ser.port     = port
     ser.dtr = 1
     ser.rts = 1
-    ser.timeout = 3
+    ser.timeout = 1
 
     ser.open()
 
     ser.dtr = 0
     ser.rts = 0
-    time.sleep(5) # Wait for device to boot
+
+    print("Waiting for device to boot")
+    time.sleep(4) # Wait for device to boot
 
     def getc(size, timeout=5):
         return ser.read(size=size)
@@ -38,12 +40,14 @@ def main(args):
 
     ymodem = YMODEM(getc, putc)
 
+    print("Initiating Upload")
     ser.write(b"upload\n")
     consume(ser)
 
+    print("Sending File")
     ymodem.send([filename,])
 
-    print("Upload Complete")
+    print("Upload Complete. If you got a 'send error: expected ACK; got b\'\\x15\'' the transfer was probably fine.")
 
 if __name__ == '__main__':
     main(sys.argv)
