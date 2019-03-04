bd37c6a878b0c8d09bda911a97daea7ec19274ea
Brian Pugh <bnp117@gmail.com>
2019-03-03 17:23:10 -0800

    remove unused fonts

--- jolt_os/jolt_gui/custom_fonts/custom_fonts.mk
+++ jolt_os/jolt_gui/custom_fonts/custom_fonts.mk
@@ -1,8 +1,6 @@
-CSRCS += f_6x10.c
-CSRCS += synchronizer7.c
 CSRCS += jolt_gui_symbols.c
-CSRCS += crox3hb.c
-CSRCS += unifont.c
+CSRCS += dejavu_40_numeric.c
+CSRCS += crox3hb_numeric.c
 DEPPATH += --dep-path jolt_gui/custom_fonts
 VPATH += :jolt_gui/custom_fonts
 CFLAGS += "-I$(LVGL_DIR)/jolt_gui/custom_fonts"
