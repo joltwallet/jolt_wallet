56bebd3ee3671725e887a5978f8fde55ea08a99c
Brian Pugh <bnp117@gmail.com>
2019-09-14 18:06:40 -0700

    Parse --passphrase argument when launching an app

--- jolt_os/syscore/launcher.c
+++ jolt_os/syscore/launcher.c
@@ -69,6 +69,9 @@ int launch_file(const char *fn_basename, int app_argc, char** app_argv, const ch
     LOADER_FD_T program = NULL;
     char *exec_fn = NULL;
 
+    ESP_LOGI(TAG, "Attempting to launching \"%s\" with passphrase \"%s\" and %d args",
+            fn_basename, passphrase, app_argc);
+
     if( true == app_cache.loading ){
         ESP_LOGW(TAG, "Cannot process additional app requests until "
                 "previous app request resolves.");
