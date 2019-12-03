d4294ecfeb267d1ab2ea18334faab925de794c3f
Brian Pugh <bnp117@gmail.com>
2019-12-03 09:07:16 -0800

    Replace some printf with log statements

--- jolt_os/syscore/cli_helpers.c
+++ jolt_os/syscore/cli_helpers.c
@@ -1,14 +1,17 @@
 #include "cli_helpers.h"
+#include "esp_log.h"
 #include "stdio.h"
+
+static const char TAG[] = "cli_helpers";
 
 bool console_check_range_argc( uint8_t argc, uint8_t min, uint8_t max )
 {
     if( argc > max ) {
-        printf( "Too many input arguments; max %d args, got %d\n", max, argc );
+        ESP_LOGE( TAG, "Too many input arguments; max %d args, got %d\n", max, argc );
         return false;
     }
     if( argc < min ) {
-        printf( "Too few input arguments; min %d args, got %d\n", min, argc );
+        ESP_LOGE( TAG, "Too few input arguments; min %d args, got %d\n", min, argc );
         return false;
     }
 
@@ -18,7 +21,7 @@ bool console_check_range_argc( uint8_t argc, uint8_t min, uint8_t max )
 bool console_check_equal_argc( uint8_t argc, uint8_t expected )
 {
     if( argc != expected ) {
-        printf( "Incorrect number of input arguments; expected %d args, got %d\n", expected, argc );
+        ESP_LOGE( TAG, "Incorrect number of input arguments; expected %d args, got %d\n", expected, argc );
         return false;
     }
     return true;
