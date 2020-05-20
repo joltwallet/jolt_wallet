6ebe14a10cc4d36a441eb2a2b8d879f9090b3990
Brian Pugh <bnp117@gmail.com>
2020-05-19 20:43:10 -0700

    Update firmware_update --patch error codes

--- jolt_os/syscore/cmd/jolt_cmd_upload_firmware.c
+++ jolt_os/syscore/cmd/jolt_cmd_upload_firmware.c
@@ -78,11 +78,11 @@ static int jolt_cmd_upload_firmware_ymodem_task( jolt_bg_job_t *job )
             char buf[64];
 
             ESP_LOGE(TAG, "Failed to apply patch");
-            snprintf( buf, sizeof( buf ), "%s=%d", gettext( JOLT_TEXT_ERROR ), err );
+            snprintf( buf, sizeof( buf ), "%s=%d", gettext( JOLT_TEXT_ERROR ), -100 );
             jolt_gui_scr_loadingbar_update( loading_scr, NULL, buf, -1 );
             /* Allow screen to be deleted via back button */
             jolt_gui_scr_set_event_cb( loading_scr, jolt_gui_event_del );
-            EXIT( -6 );
+            EXIT( -100 );
         }
     }
     else{
