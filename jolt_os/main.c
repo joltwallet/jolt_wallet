60831f0c019fe0149fbfa74da082e1773a649ff5
Brian Pugh <bnp117@gmail.com>
2020-05-20 21:16:27 -0700

    comment formatting

--- jolt_os/main.c
+++ jolt_os/main.c
@@ -54,7 +54,7 @@ static heap_trace_record_t trace_records[HEAP_TRACING_NUM_RECORDS];
 /* DO NOT directly change JOLT_OS_VERSION. It is updated via bumpversion */
 const jolt_version_t JOLT_OS_VERSION = {.major = 0, .minor = 0, .patch = 0};
 
-/* todo: this information should be stored in another partition */
+/* TODO: this information should be stored in another partition */
 const jolt_version_t JOLT_HW_VERSION = {
         .major = JOLT_HW_JOLT,
         .minor = 1,
